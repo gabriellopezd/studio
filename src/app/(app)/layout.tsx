f991f314084be35c3c7bdb3de10cb2abe7681e45
Gabriel Eduardo López de la Ossa <gabriel.lopez@cnmh.gov.co>
2025-09-25 18:29:08 +0000

    Try fixing this error: `Runtime Error: Error: React.Children.only expect

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -90,12 +90,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       </Sidebar>
       <SidebarInset>
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-          <SidebarTrigger className="sm:hidden" asChild>
-            <Button size="icon" variant="outline">
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Toggle Menu</span>
-            </Button>
-          </SidebarTrigger>
+          <SidebarTrigger className="sm:hidden" />
           <div className="relative ml-auto flex-1 md:grow-0">
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
