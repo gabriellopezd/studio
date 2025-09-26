adb9a45989c2f8e0570b83bbeb6536449b9468b0
Gabriel Eduardo López de la Ossa <gabriel.lopez@cnmh.gov.co>
2025-09-26 01:36:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3295638885-521a3",
+  "appId": "1:567524640351:web:a82dc2400fa0cdcb023dea",
+  "apiKey": "AIzaSyAqFu2RX3d_8ReHxbndVOg5cwu0UyaB5Og",
+  "authDomain": "studio-3295638885-521a3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "567524640351"
+};
