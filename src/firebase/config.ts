76f9fe4eafed05627f5c0f43af7c1527cc32eaa2
Gabriel Eduardo López de la Ossa <gabriel.lopez@cnmh.gov.co>
2025-12-07 21:50:12 +0000

    me gustaria exportar todo este proyecto a otra cuenta de google, me pued

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-3295638885-521a3",
-  "appId": "1:567524640351:web:a82dc2400fa0cdcb023dea",
-  "apiKey": "AIzaSyAqFu2RX3d_8ReHxbndVOg5cwu0UyaB5Og",
-  "authDomain": "studio-3295638885-521a3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "567524640351"
+  // PEGA AQUÍ LA CONFIGURACIÓN DE TU NUEVO PROYECTO DE FIREBASE
+  // La obtienes al registrar una nueva aplicación web en la consola de Firebase.
+  projectId: "tu-nuevo-proyecto-id",
+  appId: "tu-nuevo-app-id",
+  apiKey: "tu-nuevo-api-key",
+  authDomain: "tu-nuevo-proyecto.firebaseapp.com",
+  measurementId: "tu-nuevo-measurement-id",
+  messagingSenderId: "tu-nuevo-sender-id"
 };
